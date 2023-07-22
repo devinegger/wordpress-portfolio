7bc95ec1942e69b5207f2fedcd8eacb89e73129e
devinegger <itsme@devinegger.cc>
2023-07-22 14:06:10 -0400

    style updates for css variables

--- wp-content/plugins/destination-heading/build/index.asset.php
+++ wp-content/plugins/destination-heading/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '182a2a3e884d6d960941');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'a4add0a8e101c3085dbd');
